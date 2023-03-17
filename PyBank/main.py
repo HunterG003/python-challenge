f3cbc7ed1c932bf2ee6f72124874f76c28dc80b6
Hunterg003 <huntergilliam03@gmail.com>
2023-03-16 21:15:55 -0500

    Update main.py
    - Created function to find the average change in the list
    - Tracks and finds the greatest increase and decrease in profits
    - Added a write path to prepare to create a file to post analysis results

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,9 +2,24 @@ import os
 import csv
 
 greatestProfitIncrease = 0
+greatestProfitIncreaseMonth = ""
 greatestProfitDecrease = 0
+greatestProfitDecreaseMonth = ""
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
+writepath = os.path.join('analysis', 'financial_analysis.txt')
+
+def findAverageChange(months, nums):
+
+    numMonths = len(months)
+
+    x1 = 0
+    x2 = numMonths - 1
+
+    y1 = nums[0]
+    y2 = nums[numMonths-1]
+
+    return round((y1 - y2) / (x1 - x2), 2)
 
 with open(csvpath) as csvfile:
     csv_reader = csv.reader(csvfile)
@@ -15,22 +30,33 @@ with open(csvpath) as csvfile:
     amounts = []
 
     netTotal = 0
+    lastMonthCost = 0
 
     for row in csv_reader:
-        cost = row[1]
+        cost = int(row[1])
         month = row[0]
 
         months.append(month)
         amounts.append(cost)
 
+        change = cost - lastMonthCost
+
+        if change > greatestProfitIncrease:
+            greatestProfitIncrease = change
+            greatestProfitIncreaseMonth = month
+        elif change < greatestProfitDecrease:
+            greatestProfitDecrease = change
+            greatestProfitDecreaseMonth = month
+
         netTotal += int(cost)
+        lastMonthCost = cost
 
     totalMonths = len(months)
 
-    print("Finanical Analysis\n")
+    print("Financial Analysis\n")
     print("-----------------------------\n")
     print(f"Total Months: {totalMonths}")
     print(f"Total: ${netTotal}")
-    print(f"Average change: $")
-    print(f"Greatest Increase in Profits: ")
-    print(f"Greatest Decrease in Profits: ")
+    print(f"Average change: ${findAverageChange(months, amounts)}")
+    print(f"Greatest Increase in Profits: {greatestProfitIncreaseMonth} (${greatestProfitIncrease})")
+    print(f"Greatest Decrease in Profits: {greatestProfitDecreaseMonth} (${greatestProfitDecrease})")
